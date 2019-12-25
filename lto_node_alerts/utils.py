9e2a47a9913ae9c1e1321ee4df1372743663a692
Paulo Antunes <pjmlantunes@gmail.com>
2019-12-25 16:37:43 +0000

    Quick codefix

--- lto_node_alerts/utils.py
+++ lto_node_alerts/utils.py
@@ -2,5 +2,5 @@ import locale
 
 
 def get_number_formatted(number):
-    locale.setlocale(locale.LC_ALL, 'en_US')
+    locale.setlocale(locale.LC_ALL, 'en_US.utf8')
     return locale.format_string("%d", number, grouping=True)
