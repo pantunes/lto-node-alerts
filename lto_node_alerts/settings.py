f11cebafc48a77c2568a4248901774beaafa8fa2
Paulo Antunes <pjmlantunes@gmail.com>
2020-01-09 23:11:20 +0100

    Add more node addresses to make pooling more decentralized

--- lto_node_alerts/settings.py
+++ lto_node_alerts/settings.py
@@ -1,5 +1,8 @@
 NODES_ADDRESSES = (
     '172.104.227.247:6869',  # pauloantunes.com/lto
+    '142.93.238.193:6869',   # LowSea Leasing
+    '142.93.142.26:6869',    # Liquid Leasing Network
+    '95.216.198.5:6869',     # ZAVODIL
     # add more reliable nodes below
 )
 
